ffda4e65e2dbd1d13502d81404e4739b785b5e9d
Jan Vium Enghoff <jan.jve@gmail.com>
2015-12-18 23:03:56 +0100

    Setup Angular

---
+++ src/TfsAdministrationTool-Web/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace TfsAdministrationTool_Web
+{
+    // TODO: This should be replaced by grunt bundle + minification for consistensy - Remember to remove web.optimization reference
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/app_style")
+                .Include("~/Content/Site.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/app_script")
+                .Include("~/wwwroot/app/app.module.js")
+                .Include("~/wwwroot/app/app.routes.js")
+                .IncludeDirectory("~/wwwroot/app/main", "*.js")
+                );
+
+
+            bundles.Add(new StyleBundle("~/bundles/bower_style")
+                .Include("~/wwwroot/lib/_bower.min.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/bower_script")
+                .Include("~/wwwroot/lib/_bower.min.js")
+                );
+        }
+    }
+}
---
+++ src/TfsAdministrationTool-Web/wwwroot/app/app.routes.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app')
+        .config(config);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    function config($routeProvider, $locationProvider) {
+        //$routeProvider
+        //    .when('/routeOne', {
+        //        templateUrl: 'routesDemo/one',
+        //        controller: 'adminController',
+        //        controllerAs: 'vm'
+        //    });
+
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    }
+})();
